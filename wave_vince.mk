2e9a0ebbb919d4b2a61cc468719ae6252a21507f
Ashish Singh <82139622+deathstroke0903@users.noreply.github.com>
2021-08-09 11:50:07 +0530

    Update and rename xtended_vince.mk to wave_vince.mk

--- wave_vince.mk
+++ wave_vince.mk
@@ -28,11 +28,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
 # Inherit some common Exthm stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+$(call inherit-product, vendor/wave/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := xtended_vince
+PRODUCT_NAME := wave_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
